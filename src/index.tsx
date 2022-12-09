95c60f513228e280674cbe930c1a63de38d15e54
mfjkri <muhammadfikri6844@gmail.com>
2022-12-10 07:46:40 +0800

    Cleaned up old files

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import "./assets/css/index.css";
-// import "bootstrap/dist/css/bootstrap.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
+import "assets/css/index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
