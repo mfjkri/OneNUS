1ee7b7e27caaccda01622ee20b6004135c44d5f1
mfjkri <muhammadfikri6844@gmail.com>
2022-12-09 11:14:33 +0800

    Fixed routing for App

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import "./assets/css/App.css";
+
 import NavBar from "./components/static/navBar";
+import LoginPage from "./components/core/users/loginPage";
+
+function HelloWorld() {
+  return <p style={{ color: "white" }}>Hello World</p>;
+}
 
 function App() {
-  return <NavBar />;
+  return (
+    <React.Fragment>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<HelloWorld />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </React.Fragment>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,16 @@ import "./assets/css/index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>
 );
