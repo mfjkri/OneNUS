214429d33ff78119f693a650ac7abfe461df0e32
mfjkri <muhammadfikri6844@gmail.com>
2022-12-10 14:41:58 +0800

    Removed unused Logo component and fixed shadow on NavBar

--- src/components/Layout/MainLayout.tsx
+++ src/components/Layout/MainLayout.tsx
@@ -5,17 +5,6 @@ import { Button, Link } from "components/Elements";
 import logo from "assets/logo.svg";
 import { useAuth } from "lib/auth";
 
-// const Logo = () => {
-//   return (
-//     <Link className="flex items-center text-white" to=".">
-//       <img className="h-8 w-auto" src={logo} alt="Workflow" />
-//       <span className="text-xl text-white font-semibold">
-//         Bulletproof React
-//       </span>
-//     </Link>
-//   );
-// };
-
 type MainLayoutProps = {
   children: React.ReactNode;
 };
@@ -24,7 +13,7 @@ const NavigationBar = () => {
   const { logout, isLoggingOut } = useAuth();
 
   return (
-    <nav className="px-2 sm:px-4 py-2.5 bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+    <nav className="px-2 sm:px-4 py-2.5 bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-600">
       <div className="container flex flex-wrap items-center justify-between mx-auto">
         <Link to="/app" className="flex items-center">
           <img src={logo} className="h-6 mr-3 sm:h-9" alt="Flowbite Logo" />
@@ -49,9 +38,9 @@ const NavigationBar = () => {
 
 export const MainLayout = ({ children }: MainLayoutProps) => {
   return (
-    <div className="h-screen flex overflow-hidden bg-gray-100">
+    <div className="h-screen flex overflow-hidden">
       <div className="flex flex-col w-0 flex-1 overflow-hidden">
-        <div className="relative z-10 flex-shrink-0 flex h-16 bg-white shadow">
+        <div className="relative z-10 flex-shrink-0 flex h-16">
           <div className="flex-1 px-4 flex justify-end">
             <div className="ml-4 flex items-center md:ml-6">
               <NavigationBar />
