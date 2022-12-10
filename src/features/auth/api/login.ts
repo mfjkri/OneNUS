512bb668b21af7fcb68b98322f77f9d017f7ff9c
mfjkri <muhammadfikri6844@gmail.com>
2022-12-10 20:26:17 +0800

    Integrated Auth API for Login and GetUser

--- src/features/auth/api/login.ts
+++ src/features/auth/api/login.ts
@@ -1,23 +1,14 @@
-// import { axios } from "lib/axios";
+import { axios } from "lib/axios";
 
 import { UserResponse } from "../types";
-import { getTestUser } from "./testUser";
-
-import { delay } from "utils/delay";
-import { ARTIFICIAL_DELAY } from "config";
 
 export type LoginCredentialsDTO = {
   username: string;
   password: string;
 };
 
-async function fake_login(data: LoginCredentialsDTO) {
-  await delay(ARTIFICIAL_DELAY);
-  return getTestUser();
-}
 export const loginWithUsernameAndPassword = (
   data: LoginCredentialsDTO
 ): Promise<UserResponse> => {
-  return fake_login(data);
-  // return axios.post("/auth/login", data);
+  return axios.post("/auth/login", data);
 };
