cdc1435591ec51e583cb63871db14b65e7870c70
mfjkri <muhammadfikri6844@gmail.com>
2022-12-12 15:26:40 +0800

    Basic PostsList view completed

---
+++ src/features/posts/components/PostPreview.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+import { Link } from "components/Elements";
+
+import { Post } from "../types";
+
+export const PostPreviewColumns = [
+  {
+    title: "Title",
+    field: "title",
+  },
+  {
+    title: "Author",
+    field: "author",
+  },
+  {
+    title: "Replies",
+    field: "repliesCount",
+  },
+  {
+    title: "Last Updated",
+    field: "Last updated",
+  },
+  {
+    title: "Tags",
+    field: "tag",
+  },
+];
+
+type PostPreviewColumnsType = {
+  title: string;
+  author: string;
+  repliesCount: number;
+  updatedAt: number;
+  tag: string;
+};
+
+type PostEntryProp = {
+  postEntry: Post;
+  entryIndex: number;
+};
+
+export const PostPreview = ({ postEntry, entryIndex }: PostEntryProp) => {
+  // TODO Make column spacing fixed and truncuate text
+  let rowClasses =
+    "px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap truncate max-w-2xl ";
+
+  if (entryIndex % 2 === 0) {
+    rowClasses += "bg-secondary2 ";
+  }
+
+  return (
+    <React.Fragment>
+      <td key="Title0" className={rowClasses}>
+        <Link to="">
+          <u className="text-primary hover:text-accent">{postEntry.title}</u>
+        </Link>
+      </td>
+      <td key="Author1" className={rowClasses + "text-center"}>
+        {postEntry.author}
+      </td>
+      <td key="Replies2" className={rowClasses + "text-center"}>
+        {postEntry.repliesCount}
+      </td>
+      <td key="Last Updated3" className={rowClasses + "text-center"}>
+        {postEntry.updatedAt}
+      </td>
+      <td key="Tags4" className={rowClasses + "text-center"}>
+        {postEntry.tag}
+      </td>
+    </React.Fragment>
+  );
+};
--- src/features/posts/components/PostsList.tsx
+++ src/features/posts/components/PostsList.tsx
@@ -1,21 +1,50 @@
-import { Table } from "components/Elements";
+import { CreateFakePostsData } from "mock/posts";
 
-import { MockUser, PostsData } from "mock/posts";
+import { Post } from "../types";
+import { PostPreview } from "./PostPreview";
 
 export const PostsList = () => {
+  const data = CreateFakePostsData(100);
+  const columnClasses =
+    "px-6 py-3 text-xs font-medium tracking-wider text-left uppercase text-center ";
+
   return (
-    <Table<MockUser>
-      data={PostsData}
-      columns={[
-        {
-          title: "Username",
-          field: "username",
-        },
-        {
-          title: "ID",
-          field: "id",
-        },
-      ]}
-    />
+    <div className="flex flex-col">
+      <div className="-my-2 sm:-mx-6 lg:-mx-8">
+        <div className="inline-block w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div className="overflow-auto border-b border-primary2 shadow sm:rounded-lg">
+            <table className="table-auto min-w-full divide-y bg-secondary">
+              <thead className="bg-primary text-secondary2">
+                <tr>
+                  <th key="Title0" scope="col" className={columnClasses}></th>
+                  <th key="Author1" scope="col" className={columnClasses}>
+                    Author
+                  </th>
+                  <th key="Replies2" scope="col" className={columnClasses}>
+                    Replies
+                  </th>
+                  <th key="Last Updated3" scope="col" className={columnClasses}>
+                    Last Updated
+                  </th>
+                  <th key="Tags4" scope="col" className={columnClasses}>
+                    Tags
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((entry: Post, entryIndex: number) => (
+                  <tr
+                    key={entry?.id || entryIndex}
+                    className="odd:bg-white even:bg-gray-100"
+                  >
+                    {<PostPreview postEntry={entry} entryIndex={entryIndex} />}
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
