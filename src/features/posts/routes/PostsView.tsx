44c14e965001bcf1fcf176b0de73f6aade4ca5b2
mfjkri <muhammadfikri6844@gmail.com>
2022-12-12 15:35:39 +0800

    Added placeholder button for new post

--- src/features/posts/routes/PostsView.tsx
+++ src/features/posts/routes/PostsView.tsx
@@ -1,3 +1,6 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+
+import { Button } from "components/Elements";
 import { ContentLayout } from "components/Layout";
 
 import { PostsList } from "../components/PostsList";
@@ -5,6 +8,12 @@ import { PostsList } from "../components/PostsList";
 export const PostsView = () => {
   return (
     <ContentLayout title="">
+      <Button
+        className="mb-3"
+        startIcon={<PlusIcon className="h-6 w-6" aria-hidden="true" />}
+      >
+        New Post
+      </Button>
       <PostsList />
     </ContentLayout>
   );
