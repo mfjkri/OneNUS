c4db0150b0f675c0bdb31449b9be6a26ac654811
mfjkri <muhammadfikri6844@gmail.com>
2022-12-12 15:24:20 +0800

    Using faker to generate mock posts data

--- src/mock/posts/index.tsx
+++ src/mock/posts/index.tsx
@@ -1,15 +1,48 @@
-export type MockUser = {
-  id: string;
-  username: string;
+import { faker } from "@faker-js/faker";
+
+import { Post } from "features/posts";
+
+function randomIntFromInterval(min: number, max: number) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+export const CreateFakePostsData = (n: number): Post[] => {
+  const currData: Post[] = [];
+  for (let i = 0; i < n; i = i + 1) {
+    currData.push({
+      id: String(i),
+      title: faker.lorem.sentence(randomIntFromInterval(4, 10)),
+      tag: faker.lorem.word(),
+      text: faker.lorem.sentences(randomIntFromInterval(4, 10)),
+      author: faker.name.firstName(),
+      repliesCount: randomIntFromInterval(0, 40),
+      updatedAt: 10,
+      createdAt: 10,
+    });
+  }
+  return currData;
 };
 
-export const PostsData: MockUser[] = [
+export const DefaultFakePostsData: Post[] = [
   {
     id: "1",
-    username: "Johnny Smith",
+    title: "Lorem ipsum dolor sit",
+    tag: "misc",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, harum? Magnam assumenda atque maxime unde animi reprehenderit nobis minus architecto, delectus deserunt aliquam quibusdam vitae iste excepturi nostrum earum asperiores, et velit nulla! Earum fuga enim, eveniet culpa nemo odit ut cum minima! Ab ducimus possimus voluptates quibusdam molestiae, pariatur tempore. Qui minus at numquam unde exercitationem suscipit, hic amet expedita assumenda. Porro fugit, est voluptates illum quis dolorem magni cupiditate quas aliquam aliquid nam facere beatae vitae exercitationem dicta tenetur sunt voluptatibus doloremque corporis. Veniam, quasi doloremque eveniet, tenetur recusandae quibusdam sequi dicta corrupti iure sint debitis, praesentium quo?",
+    author: "nobody",
+    repliesCount: 2,
+    updatedAt: 10,
+    createdAt: 10,
   },
   {
     id: "2",
-    username: "Cody Fisher",
+    title: "Another reason why",
+    tag: "misc",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deserunt, harum? Magnam assumenda atque maxime unde animi reprehenderit nobis minus architecto, delectus deserunt aliquam quibusdam vitae iste excepturi nostrum earum asperiores, et velit nulla! Earum fuga enim, eveniet culpa nemo odit ut cum minima! Ab ducimus possimus voluptates quibusdam molestiae, pariatur tempore. Qui minus at numquam unde exercitationem suscipit, hic amet expedita assumenda. Porro fugit, est voluptates illum quis dolorem magni cupiditate quas aliquam aliquid nam facere beatae vitae exercitationem dicta tenetur sunt voluptatibus doloremque corporis. Veniam, quasi doloremque eveniet, tenetur recusandae quibusdam sequi dicta corrupti iure sint debitis, praesentium quo?",
+    author: "sombody",
+    repliesCount: 5,
+    updatedAt: 100,
+    createdAt: 100,
   },
 ];
