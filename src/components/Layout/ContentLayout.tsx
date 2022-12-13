1e31b5069301e40540448925e7ae463822893105
mfjkri <muhammadfikri6844@gmail.com>
2022-12-13 21:02:41 +0800

    Set default white text color in ContentLayout

--- src/components/Layout/ContentLayout.tsx
+++ src/components/Layout/ContentLayout.tsx
@@ -11,9 +11,9 @@ export const ContentLayout = ({ children, title }: ContentLayoutProps) => {
   return (
     <>
       <Head title={title} />
-      <div className="py-6">
+      <div className="py-6 text-secondary">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-          <h1 className="text-2xl font-semibold text-secondary">
+          <h1 className="text-2xl font-semibold">
             <u>{title}</u>
           </h1>
         </div>
