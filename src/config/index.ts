1f2864a01aefb040f8bedfdedbf36f3a6231447f
mfjkri <muhammadfikri6844@gmail.com>
2022-12-17 20:46:54 +0800

    Moved posts and comments config to .env

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,9 +4,19 @@ export const API_URL =
     ? "http://localhost:8000"
     : (process.env.REACT_APP_API_URL as string);
 
-export const MAX_POST_TITLE_CHAR = 100;
-export const MAX_POST_TEXT_CHAR = 5000;
-export const POSTS_PER_PAGE = 5;
+export const MAX_POST_TITLE_CHAR = parseInt(
+  process.env.REACT_APP_MAX_POST_TITLE_CHAR || "100"
+);
+export const MAX_POST_TEXT_CHAR = parseInt(
+  process.env.REACT_APP_MAX_POST_TEXT_CHAR || "5000"
+);
+export const POSTS_PER_PAGE = parseInt(
+  process.env.REACT_APP_POSTS_PER_PAGE || "10"
+);
 
-export const MAX_COMMENT_TEXT_CHAR = 1000;
-export const COMMENTS_PER_PAGE = 10;
+export const MAX_COMMENT_TEXT_CHAR = parseInt(
+  process.env.REACT_APP_MAX_COMMENT_TEXT_CHAR || "1000"
+);
+export const COMMENTS_PER_PAGE = parseInt(
+  process.env.REACT_APP_COMMENTS_PER_PAGE || "10"
+);
