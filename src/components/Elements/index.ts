5877d0209170988f3c4dd746f5d6e5a3c2bc6edd
mfjkri <muhammadfikri6844@gmail.com>
2022-12-19 10:25:56 +0800

    Removed unused components

--- src/components/Elements/index.ts
+++ src/components/Elements/index.ts
@@ -1,7 +1,5 @@
 export * from "./Button";
 export * from "./ConfirmationDialog";
 export * from "./Dialog";
-export * from "./Drawer";
 export * from "./Link";
 export * from "./Spinner";
-export * from "./Table";
--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,5 +1,4 @@
 export * from "./Form";
-export * from "./FormDrawer";
 export * from "./InputField";
 export * from "./SelectField";
 export * from "./TextareaField";
