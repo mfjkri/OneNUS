a6176e95ff203303aa6000dedfa8da731c7ec7c8
mfjkri <muhammadfikri6844@gmail.com>
2022-12-23 19:33:50 +0800

    wip: starring post now has visual feedback

--- src/features/posts/components/StarPost.tsx
+++ src/features/posts/components/StarPost.tsx
@@ -1,11 +1,22 @@
+import clsx from "clsx";
 import { StarIcon } from "@heroicons/react/24/outline";
 
+import { useDisclosure } from "hooks/useDisclosure";
+
 export const StarPost = ({ starsCount }: { starsCount: number }) => {
-  // return <div></div>;
+  const { isOpen: isStarred, toggle } = useDisclosure(false);
+
   return (
-    <div className="flex flex-col">
-      <StarIcon className="h-6 w-auto hover:fill-orange-400 hover:cursor-pointer" />
-      <p className="text-center text-[12px]">{starsCount}</p>
+    <div className="flex flex-col" onClick={toggle}>
+      <StarIcon
+        className={clsx(
+          "h-6 w-auto hover:fill-orange-400 hover:cursor-pointer",
+          isStarred && "fill-orange-400"
+        )}
+      />
+      <p className="text-center text-[12px]">
+        {isStarred ? starsCount + 1 : starsCount}
+      </p>
     </div>
   );
 };
