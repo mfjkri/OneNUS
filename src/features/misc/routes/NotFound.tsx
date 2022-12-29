33a4a04edf55c07bd351bf5e9919c32491c3bb84
mfjkri <muhammadfikri6844@gmail.com>
2022-12-30 07:38:52 +0800

    feat: added emoticon for NotFound page

--- src/features/misc/routes/NotFound.tsx
+++ src/features/misc/routes/NotFound.tsx
@@ -1,3 +1,5 @@
+import { FaceFrownIcon } from "@heroicons/react/24/outline";
+
 import logo from "assets/logo.svg";
 import { Button, Link } from "components/Elements";
 
@@ -10,9 +12,15 @@ export const NotFound = () => {
       <div className="h-[100vh] flex items-center text-secondary">
         <div className="max-w-7xl mx-auto text-center py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
           <img className="w-full my-3" src={logo} alt="react" />
-          <h2 className="text-3xl font-extrabold tracking-tight sm:text-4xl">
-            <span className="block mt-4">PAGE NOT FOUND!</span>
-          </h2>
+          <div className="flex flex-row">
+            <FaceFrownIcon
+              className="h-12 w-auto my-auto mr-2"
+              aria-hidden="true"
+            />
+            <h2 className="my-auto text-3xl font-extrabold sm:text-4xl">
+              PAGE NOT FOUND!
+            </h2>
+          </div>
 
           <div className="mt-8 flex justify-center">
             <div className="inline-flex rounded-md shadow">
