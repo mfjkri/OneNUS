6cf3ad50c2285e53003dfb193d35eeabfd01dbc2
mfjkri <muhammadfikri6844@gmail.com>
2022-12-30 07:39:21 +0800

    fix: use memoized callback for onSuccess

--- src/features/posts/routes/New.tsx
+++ src/features/posts/routes/New.tsx
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { ContentLayout } from "components/Layout";
@@ -10,16 +11,16 @@ export const NewPost = () => {
   const navigate = useNavigate();
   const dispatch = useAppDispatch();
 
+  const onSuccess = useCallback(() => {
+    dispatch(resetState());
+    navigate("/app/posts");
+  }, [navigate, dispatch]);
+
   return (
     <ContentLayout title="">
       <h2 className="text-center text-3xl font-extrabold">New Post</h2>
       <div className="bg-secondary dark:bg-primary mt-8 py-8 shadow rounded-3xl px-10">
-        <CreatePostForm
-          onSuccess={() => {
-            dispatch(resetState());
-            navigate("/app/posts");
-          }}
-        />
+        <CreatePostForm onSuccess={onSuccess} />
       </div>
     </ContentLayout>
   );
